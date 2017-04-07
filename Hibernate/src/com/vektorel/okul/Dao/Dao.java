553996cbd1f28819c095d8ffac23251bbaa4add9
mustafa.ergan <mustafa.ergan@enerjisa.com>
2017-04-07 09:01:11 +0300

    transaction

--- Hibernate/src/com/vektorel/okul/Dao/Dao.java
+++ Hibernate/src/com/vektorel/okul/Dao/Dao.java
@@ -1,7 +1,9 @@
 package com.vektorel.okul.Dao;
 
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
 import com.vektorel.okul.entity.Mudur;
@@ -9,42 +11,59 @@ import com.vektorel.okul.entity.Student;
 
 public class Dao {
 
-	Session session;
+
+	SessionFactory sessionFactory;
 
 	public Dao() {
 		SessionFactory sessionFactory = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
-		session = sessionFactory.openSession();
 	}
 
 	public void addStudent(Student student) {
-		session.save(student);
-		session.beginTransaction().commit();
-	}
-
-	public Student getStudent(String id) {
-		Student student = session.get(Student.class, id);
-		return student;
+		Session session = sessionFactory.openSession();
+		Transaction tx = null;
+		try{
+			tx = session.beginTransaction();
+			session.save(student);
+			tx.commit();
+		}catch (HibernateException e) {
+			if (tx!=null) tx.rollback();
+			e.printStackTrace();
+		}finally {
+			session.close();
+		}
 	}
 
 	public Student getStudent(int id) {
-		Student student = session.get(Student.class, String.valueOf(id));
-		return student;
+		Session session = sessionFactory.openSession();
+		Transaction tx = null;
+		Student student1 = null;
+		try{
+			tx = session.beginTransaction();
+			student1 = session.get(Student.class,id);
+			tx.commit();
+		}catch (HibernateException e) {
+			if (tx!=null) tx.rollback();
+			e.printStackTrace();
+		}finally {
+			session.close();
+		}
+		return student1;
 	}
 
-	public void addMudur(Mudur mudur) {
-		session.save(mudur);
-		session.beginTransaction().commit();
-	}
-
-	public Mudur getMudur(int id) {
-		
-		session.beginTransaction().begin();
-		Mudur mudur = session.get(Mudur.class, String.valueOf(id));
-		return mudur;
-	}
-
-	public void close() {
-		session.close();
-	}
+//	public void addMudur(Mudur mudur) {
+//		session.save(mudur);
+//		session.beginTransaction().commit();
+//	}
+//
+//	public Mudur getMudur(int id) {
+//
+//		session.beginTransaction().begin();
+//		Mudur mudur = session.get(Mudur.class, String.valueOf(id));
+//		return mudur;
+//	}
+//
+//	public void close() {
+//		session.close();
+//	}
 
 }
