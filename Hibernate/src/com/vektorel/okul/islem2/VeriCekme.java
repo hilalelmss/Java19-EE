6cdcc32c9706874ffa836ca49eaa5cf147b5897b
mustafa.ergan <mustafa.ergan@enerjisa.com>
2017-04-07 08:57:21 +0300

    trancastion

---
+++ Hibernate/src/com/vektorel/okul/islem2/VeriCekme.java
@@ -0,0 +1,18 @@
+package com.vektorel.okul.islem;
+
+import com.vektorel.okul.Dao.Dao;
+import com.vektorel.okul.entity.Student;
+
+public class VeriCekme {
+	
+	public static void main(String[] args) {
+
+		Dao dao = new Dao();
+
+		Student student = dao.getStudent(2);
+
+		System.out.println(student.getName());
+
+	}
+
+}
---
+++ Hibernate/src/com/vektorel/okul/islem2/VeriEkle.java
@@ -0,0 +1,19 @@
+package com.vektorel.okul.islem;
+
+import com.vektorel.okul.Dao.Dao;
+import com.vektorel.okul.entity.Student;
+
+public class VeriEkle {
+	
+	public static void main(String[] args) {
+		Dao dao = new Dao();
+
+		Student student = new Student();
+
+		student.setId(2);
+		student.setName("Mustafak");
+
+		dao.addStudent(student);
+	}
+
+}
