10937dcb409e6f4f2600c98229bddb6eecab5978
mustafa.ergan <mustafa.ergan@enerjisa.com>
2017-04-21 13:32:48 +0300

    criteria

---
+++ JSF/src/main/java/otopark/Dede.java
@@ -0,0 +1,14 @@
+package otopark;
+
+public class Dede {
+	
+	String name = "mustafa";
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+}
